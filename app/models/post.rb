2cac8c2add00523f92e3af5ac80b36258e806d22
Chris Swann <chris@interludedesign.com>
2019-03-29 13:50:35 -0700

    Add queries and display in view

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,19 @@ class Post < ApplicationRecord
 		joins(:approval)
 	end
 
+	def self.not_requiring_approval
+		where.not(id: Approval.select(:approvable_id))
+	end
+
 	def self.approved
 		requiring_approval.merge Approval.approved
 	end
 
 	def self.pending
 		requiring_approval.merge Approval.pending
+	end
+
+	def approval_state
+		try(:approval).try(:state) || :not_required
 	end
 end
